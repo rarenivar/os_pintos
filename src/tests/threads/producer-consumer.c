90eb082f7a60a89366c5115f605e78ab31f19764
Ramiro Arenivar <ramiro.arenivar@gmail.com>
2015-10-24 19:42:23 -0600

    refactor code, code organization, order and added comments

--- src/tests/threads/producer-consumer.c
+++ src/tests/threads/producer-consumer.c
@@ -1,84 +1,47 @@
-/*
- * Tests producer/consumer communication with different numbers of threads.
- * Automatic checks only catch severe problems like crashes.
- */
-
+// producer consumer program
 #include <stdio.h>
-#include "tests/threads/tests.h"
+#include "threads/thread.h"
 #include "threads/malloc.h"
 #include "threads/synch.h"
-#include "threads/thread.h"
-#include "lib/kernel/list.h"
-#include "devices/timer.h"
+#include "tests/threads/tests.h"
 
 #define PRIORITY 0
 #define DATASIZE 5
 
+// condition variables structs, defined in synch.h
+static struct condition notFull;
+static struct condition notEmpty;
+// for the buffer
+static char buffer[DATASIZE];
+// data buffer
+char consumerData[] = "hello";
+// buffer positions
+static int head = 0;
+static int tail = 0;
+// lock
+static struct lock mutex;
 
 void GenerateProducerConsumer(int numberOfProducers,int numberOfConsumers);
 void Producer_func(void *aux UNUSED);
 void Consumer_func(void *aux UNUSED);
 
-// data buffer
-char  consumerData[] = "hello";
-
-// for the buffer
-static char buffer[DATASIZE];
-
-// buffer position
-static int head = 0;
-static int tail = 0;
-static int wrap = 0;
-
-// lock
-static struct lock mutex;
-
-// condition variables structs, defined in synch.h
-static struct condition notFull;
-static struct condition notEmpty;
-
 // test method
-void
-test_producer_consumer(void)
-{
-    // lock
+void test_producer_consumer(void) {
+
+    // initializing lock and conditions
     lock_init(&mutex);
-    // conditions
     cond_init(&notEmpty);
     cond_init(&notFull);
 
-    /*GenerateProducerConsumer(0, 0);
-    GenerateProducerConsumer(1, 0);
-    GenerateProducerConsumer(0, 1);
-    GenerateProducerConsumer(1, 1);
-    GenerateProducerConsumer(3, 1);
-    GenerateProducerConsumer(1, 3);
-    GenerateProducerConsumer(4, 4);
-    GenerateProducerConsumer(7, 2);
-    GenerateProducerConsumer(2, 7);*/
+    GenerateProducerConsumer(2, 7);
     GenerateProducerConsumer(6, 6);
+
+    // if no errors, the tests passed
     pass();
 }
 
 // generate the threads for the consumers and producers
-void
-GenerateProducerConsumer(int numberOfProducers, int numberOfConsumers)
-{
-    /*
-     Function:
-     tid_t thread_create (const char *name, int priority, thread_func *func, void *aux)
-     Creates and starts a new thread named name with the given priority, returning the new thread's tid.
-     The thread executes func, passing aux as the function's single argument.
-
-     thread_create() allocates a page for the thread's struct thread and stack and initializes its members,
-     then it sets up a set of fake stack frames for it (see section A.2.3 Thread Switching). The thread is
-     initialized in the blocked state, then unblocked just before returning, which allows the new thread to
-     be scheduled (see Thread States).
-
-     Type: void thread_func (void *aux)
-     This is the type of the function passed to thread_create(), whose aux argument is passed along as the
-     function's argument.
-    */
+void GenerateProducerConsumer(int numberOfProducers, int numberOfConsumers) {
     int i;
     //we'll now create the threads for the producers and consumers
     for(i = 0; i < numberOfProducers; i++)
@@ -91,134 +54,71 @@ GenerateProducerConsumer(int numberOfProducers, int numberOfConsumers)
     }
 }
 
+// Function that creates consumer threads
+void Consumer_func(void *aux UNUSED) {
 
-/*
- * Consumes all characters placed in the buffer
- * buffer, and waits if there is nothing more to consumer.
- */
-void
-Consumer_func(void *aux UNUSED){
+    int once = 0;
     char character;
 
-    /* as long as there are characters in the buffer */
-    while(true)
-    {
-        // this is the start of the thread critical section
+    while(true) {
+        // acquire lock
         lock_acquire(&mutex);
-
-        //msg("consumer_id %i", thread_tid());
-
-        /* check if there is a character left in the buffer */
-        while (head == tail && wrap == 0)
+        // make sure there is data in the buffer
+        while (head == tail)
         {
-            /* if no character is left, inform the producer threads
-             to get work done and wait until a character has been placed */
-
-            /* Wakes up all threads, if any, waiting on COND (protected by
-               LOCK).  LOCK must be held before calling this function.
-
-               An interrupt handler cannot acquire a lock, so it does not
-               make sense to try to signal a condition variable within an
-               interrupt handler. */
+            if (once == 0) {
+                msg("Consumer ID: %i", thread_tid());
+                once = 1;
+            }
+            // wakes up all threads
             cond_broadcast(&notFull, &mutex);
-            /* Atomically releases LOCK and waits for COND to be signaled by
-               some other piece of code.  After COND is signaled, LOCK is
-               reacquired before returning.  LOCK must be held before calling
-               this function.
-
-               The monitor implemented by this function is "Mesa" style, not
-               "Hoare" style, that is, sending and receiving a signal are not
-               an atomic operation.  Thus, typically the caller must recheck
-               the condition after the wait completes and, if necessary, wait
-               again.
-
-               A given condition variable is associated with only a single
-               lock, but one lock may be associated with any number of
-               condition variables.  That is, there is a one-to-many mapping
-               from locks to condition variables.
-
-               This function may sleep, so it must not be called within an
-               interrupt handler.  This function may be called with
-               interrupts disabled, but interrupts will be turned back on if
-               we need to sleep. */
             cond_wait(&notEmpty, &mutex);
         }
 
-        /* take a character from the buffer */
+        // grab data from the buffer and update tail pointer
         character = buffer[tail];
-
-        /* update read pointer */
         tail = (tail+1) % DATASIZE;
-
-        /* check if we have had a wrap-around */
-        if(tail == 0) {
-            wrap--;
-        }
-
-        /* check that there isn't more than a single wrap-around
-         of the head pointer */
-        ASSERT(wrap == 0 || wrap == 1);
-
-        /* inform the producer threads that the buffer is not full (anymore) */
+        // broadcast that the buffer is not full and release lock
         cond_broadcast(&notFull, &mutex);
-
-        /* leave the critical section */
         lock_release(&mutex);
     }
 };
 
-/*
- * Produces characters based on the string text
- * and puts them into the buffer buffer.
- *
- * Terminates when all characters have been placed in the buffer.
- */
+// Function that creates consumer threads
 void
-Producer_func(void *aux UNUSED){
-    /* local variables */
+Producer_func(void *aux UNUSED) {
+
+    int once = 0;
     int index = 0;
     char character;
 
-    /* as long as we have characters to produce */
+    // check to see that there is a character in the data array
     while( consumerData[index] != '\0'){
 
-        /* enter critical section */
+        // acquire lock
         lock_acquire(&mutex);
 
-        //msg("producer_id %i", thread_tid());
-
-        /* check if buffer is full */
-        while (head == tail && wrap == 1)
+        // broadcast if the buffer is not empty
+        // so the consumer can take an item
+        while (head == tail)
         {
-            /* state that the buffer is not empty and wait
-              for consumer to free a slot of the buffer */
+            if (once == 0) {
+                msg("Producer ID: %i", thread_tid());
+                once = 1;
+            }
             cond_broadcast(&notEmpty, &mutex);
             cond_wait(&notFull, &mutex);
         }
 
-        /* fetch the next character from the text
-         and save it to the buffer */
+        // save the data to the buffer
         character =  consumerData[index];
         buffer[head] = character;
-
-        /* update the writer & reader indexs */
-        head = (head + 1) % DATASIZE;
+        // update the pointers
         index++;
-
-        /* check if we have a wrap-around */
-        if(head == 0) {
-            wrap++;
-        }
-
-        /* assert that the head can only be one
-         wrap-around in front of tail */
-        ASSERT(wrap == 0 || wrap == 1);
-
-        /* wake up consumers, state that the buffer is not
-         empty any more */
+        head = (head + 1) % DATASIZE;
+        // need to broadcast that the buffer is not empty
         cond_broadcast(&notEmpty, &mutex);
-
-        /* leave critical section */
+        // release the lock
         lock_release(&mutex);
     }
 };
