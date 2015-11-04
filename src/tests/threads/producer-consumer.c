c0927216afe64ebd9e54cb57082befa3cda35b76
Ramiro Arenivar <ramiro.arenivar@gmail.com>
2015-11-04 15:04:27 -0700

    renamed some variables for readability

--- src/tests/threads/producer-consumer.c
+++ src/tests/threads/producer-consumer.c
@@ -15,7 +15,7 @@
 static struct condition shareBufferNotFull;
 static struct condition shareBufferNotEmpty;
 static int shareBuffer[DATASIZE];
-static int isBufferFull = 0;
+static int resetBufferCurrentIndex = 0;
 static int consumer_digit = 0;
 static int producer_digit = 0;
 
@@ -32,8 +32,8 @@ static int consumerData[PRODUCERDATASIZE] = {
         7,5,2,3,
         2,2,9,3 }; //== 215
 
-static int head = 0;
-static int tail = 0;
+static int headQueue = 0;
+static int tailQueue = 0;
 static int totalSum = 0;
 static struct lock theLock;
 static int theOrder = 0;
@@ -73,19 +73,17 @@ void Consumer_func(void *aux) {
             thread_exit();
         }
 
-        msg("Consumer ID: %i acquired lock, tail %d, isBufferFull %d", thread_tid(), tail, isBufferFull);
-        while (head == tail && isBufferFull == 0)
+        msg("Consumer ID: %i acquired lock, tailQueue %d, resetBufferCurrentIndex %d", thread_tid(), tailQueue, resetBufferCurrentIndex);
+        while (headQueue == tailQueue && resetBufferCurrentIndex == 0)
         {
             //msg("Consumer %i, butter is empty", thread_tid());
             cond_broadcast(&shareBufferNotFull, &theLock);
             cond_wait(&shareBufferNotEmpty, &theLock);
         }
-        consumer_digit = shareBuffer[tail];
-        tail = (tail+1) % DATASIZE;
+        consumer_digit = shareBuffer[tailQueue];
+        tailQueue = (tailQueue+1) % DATASIZE;
         totalSum += consumer_digit;
-        if(tail == 0) {
-            isBufferFull--;
-        }
+        if(tailQueue == 0) { resetBufferCurrentIndex = 0; }
         msg("Consumer %i grabbed: %d, Sum = %d", thread_tid(), consumer_digit, totalSum);
         cond_broadcast(&shareBufferNotFull, &theLock);
         thread_print_metrics();
@@ -102,12 +100,13 @@ void Producer_func(void *aux) {
         if (totalSum == 215) {
             thread_exit();
         }
+
         if (producer_order == 0) {
             msg("Producer ID %i was the %i producer to start", thread_tid(), theOrder);
             producer_order = theOrder;
             theOrder++;
         }
-        while (head == tail && isBufferFull == 1)
+        while (headQueue == tailQueue && resetBufferCurrentIndex == 1)
         {
             msg("Producer ID %i, broadcasting shareBuffer is not empty...", thread_tid());
             cond_broadcast(&shareBufferNotEmpty, &theLock);
@@ -115,17 +114,15 @@ void Producer_func(void *aux) {
         }
         if (producer_order < (PRODUCERDATASIZE - 1)) {
             producer_digit = consumerData[producer_order];
-            shareBuffer[head] = producer_digit;
-            //msg("It produced %i at the shareBuffer index of %i", producer_digit, head);
-            head = (head + 1) % DATASIZE;
+            shareBuffer[headQueue] = producer_digit;
+            //msg("It produced %i at the shareBuffer index of %i", producer_digit, headQueue);
+            headQueue = (headQueue + 1) % DATASIZE;
             //msg("Producer ID %i has producer_order index of %i", thread_tid(), producer_order);
             producer_order = producer_order + NUMPRODUCERS;
-            if (head == 0) {
-                isBufferFull++;
-            }
+            if (headQueue == 0) { resetBufferCurrentIndex = 1; }
         }
         cond_broadcast(&shareBufferNotEmpty, &theLock);
-        thread_print_metrics();
         lock_release(&theLock);
     }
+    thread_print_metrics();
 };
